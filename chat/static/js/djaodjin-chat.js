268a6d457a49abbe64c1d7b7cf4e704d6ef064bc
Adrian Smith <smith.adriane@gmail.com>
2016-08-26 13:00:01 -0700

    Convert chat interface js code and html code into jquery plugins.

--- chat/static/js/djaodjin-chat.js
+++ chat/static/js/djaodjin-chat.js
@@ -1,49 +1,79 @@
-// Note that the path doesn't matter for routing; any WebSocket
-// connection gets bumped over to WebSocket consumers
-
-var chatApi = new ChatApi();
-chatApi.on('connect', function(){
-    chatApi.subscribe();
-});
-
-var $chatSend = $('#chatsend');
-var $chatInput = $('#chatinput');
-var $chatLog = $('#chatlog');
-
-chatApi.on('message_from', function(message){
-    var $message = $('<div/>');
-    $message.text(message.from + ': ' + message.text);
-
-    $chatLog.append($message);
-});
-chatApi.on('message', function(message){
-    var $message = $('<div/>');
-    $message.text('me: ' + message);
-
-    $chatLog.append($message);
-});
+(function ($) {
+    "use strict";
 
 
-function sendMessage(message){
-    // var $nextMessage = $('<div/>');
-    // $nextMessage.text(message);
-    // $chatLog.append($nextMessage);
-}
+    function DjChat(el, options){
+        this.$root = $(el);
+        this.options = options;
+        this.init();
 
-function processMessageSubmit(){
-    var message = $chatInput.val();
-
-    chatApi.send(message)
-    $chatInput.val('');
-}
-
-
-$chatSend.on('click', processMessageSubmit);
-$chatInput.on('keydown', function (event){
-    if (event.which == 13 || event.keyCode == 13) {
-        event.preventDefault();
-        processMessageSubmit();
-        return false;
     }
-    return true;
-});
+
+    DjChat.prototype = {
+        init: function(){
+            var self = this;
+
+            self.$chatSend = self.findPrefixed('chatsend');
+            self.$chatInput = self.findPrefixed('chatinput');
+            self.$chatLog = self.findPrefixed('chatlog');
+
+            console.log(self.$chatSend);
+
+            self.chatApi = new ChatApi();
+            self.chatApi.on('connect', function(){
+                self.chatApi.subscribe();
+            });
+
+
+            self.chatApi.on('message_from', function(message){
+                var $message = $('<div/>');
+                $message.addClass(self.options.cssPrefix + 'chatmessage')
+                $message.text(message.from + ': ' + message.text);
+
+                self.$chatLog.append($message);
+            });
+            self.chatApi.on('message', function(message){
+                var $message = $('<div/>');
+                $message.addClass(self.options.cssPrefix + 'chatmessage')
+                $message.text('me: ' + message);
+
+                self.$chatLog.append($message);
+            });
+
+
+            self.$chatSend.on('click', self.processMessageSubmit);
+            self.$chatInput.on('keydown', function (event){
+                if (event.which == 13 || event.keyCode == 13) {
+                    event.preventDefault();
+                    self.processMessageSubmit();
+                    return false;
+                }
+                return true;
+            });
+
+        },
+        processMessageSubmit: function(){
+            var self = this;
+            var message = self.$chatInput.val();
+
+            self.chatApi.send(message)
+            self.$chatInput.val('');
+        },
+        findPrefixed: function(name){
+
+            var selector = '.' + this.options.cssPrefix + name;
+            console.log(selector)
+            console.log(this.$root);
+            return this.$root.find(selector);
+        }
+    }
+
+    $.fn.djchat = function(options) {
+        var opts = $.extend( {}, $.fn.djchat.defaults, options );
+        return new DjChat($(this), opts);
+    };
+
+    $.fn.djchat.defaults = {
+        cssPrefix: 'djaodjin-chat-',
+    };
+})(jQuery);
